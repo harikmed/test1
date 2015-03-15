ea5cae92cc95db510617c1ff687bb3f6132a186a
harik <hamouda.form@gmail.com>
2015-03-15 18:13:59 +0000

    creation users avec mysql2

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :nom, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+My2015::Application.config.secret_key_base = 'af1572193c984332f1786ee21e0cc15f0e85247071732d88f7ddfcb9fda526570ed410e4e928c49b77fe0cc6de51e0f0ac1676103bc4dd649acfd20fa4d76deb'
